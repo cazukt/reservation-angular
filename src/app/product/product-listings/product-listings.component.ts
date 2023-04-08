a12a566aaafc3c1f162ac1d205b574187e850e88
k.yoneyama <cazu_kt@yahoo.co.jp>
2023-04-08 20:07:56 +0900

    Angularフロントエンド側作成

--- src/app/product/product-listings/product-listings.component.ts
+++ src/app/product/product-listings/product-listings.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { products } from '../../products';
 
 @Component({
   selector: 'app-product-listings',
   templateUrl: './product-listings.component.html',
   styleUrls: ['./product-listings.component.scss']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit{
   //products:any=new Array(4)
   //products:number[]=[1,2,3,4]
-  products:any=[1,2,3,4]
+  products:any
+
+  constructor(){ }
+
+  ngOnInit(){
+    this.products=products
+  }
+
 }
