968959b642b1e86881fa86402865e326865be83d
k.yoneyama <cazu_kt@yahoo.co.jp>
2023-04-18 18:19:49 +0900

    ローカルストレージにデータ保存

--- src/app/auth/register/shared/auth.service.ts
+++ src/app/auth/register/shared/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs'
+import { map } from 'rxjs/operators'
 
 @Injectable()
 export class AuthService {
@@ -14,7 +15,15 @@ export class AuthService {
   register(userData: any): Observable<any> {
     return this.http.post('/api/v1/users/register', userData)
   }
+
   login(userData: any): Observable<any> {
-    return this.http.post('/api/v1/users/login', userData)
+    return this.http.post('/api/v1/users/login', userData).pipe(
+      map((token) => {
+        debugger
+        localStorage.setItem('app-auth', JSON.stringify(token))
+        return token
+      })
+    )
   }
+
 }
